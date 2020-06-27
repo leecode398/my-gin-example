ac65b4b1a03af3e8b7eeeb33c90005d89e86fd5b
lixiang-code <1411192593@qq.com>
2020-06-27 22:37:11 +0800

    new go-gin-example add write setting file

---
+++ pkg/setting/setting.go
@@ -0,0 +1,69 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-27 22:17:13
+ * @LastEditTime: 2020-06-27 22:31:53
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /go-gin-example/pkg/setting/setting.go
+ */
+package setting
+
+import (
+	"log"
+	"time"
+
+	"github.com/go-ini/ini"
+)
+
+var (
+	Cfg *ini.File
+
+	RunMode string
+
+	HTTPPort     int
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+
+	PageSize  int
+	JwtSecret string
+)
+
+func init() {
+	var err error
+	Cfg, err = ini.Load("conf/app.ini")
+	if err != nil {
+		log.Fatalf("Fail to parse 'conf/app.ini': %v", err)
+	}
+
+	LoadBase()
+	LoadServer()
+	LoadApp()
+}
+
+// LoadBase load runmode
+func LoadBase() {
+	RunMode = Cfg.Section("").Key("RUN_MODE").MustString("debug")
+}
+
+// LoadServer load server info
+func LoadServer() {
+	sec, err := Cfg.GetSection("server")
+	if err != nil {
+		log.Fatalf("Fail to get section 'server': %v", err)
+	}
+
+	HTTPPort = sec.Key("HTTP_PORT").MustInt(8000)
+	ReadTimeout = time.Duration(sec.Key("READ_TIMEOUT").MustInt(60) * time.Now().Second())
+	WriteTimeout = time.Duration(sec.Key("WRITE_TIMEOUT").MustInt(60) * time.Now().Second())
+}
+
+// LoadApp load app info
+func LoadApp() {
+	sec, err := Cfg.GetSection("app")
+	if err != nil {
+		log.Fatalf("Fail to get section 'app': %v", err)
+	}
+
+	JwtSecret = sec.Key("JWT_SECRET").MustString("!@)*#)!@U#@*!@!)")
+	PageSize = sec.Key("PAGE_SIZE").MustInt(10)
+}
