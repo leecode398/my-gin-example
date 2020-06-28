d42fb8d36c308522394e2b53c300a5b0d66151e2
lixiang-code <1411192593@qq.com>
2020-06-28 22:34:31 +0800

    编写路由和标签表的逻辑

---
+++ routers/router.go
@@ -0,0 +1,27 @@
+package routers
+
+import (
+	"github.com/EDDYCJY/go-gin-example/pkg/setting"
+	v1 "github.com/EDDYCJY/go-gin-example/routers/api/v1"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() *gin.Engine {
+	r := gin.New()
+
+	r.Use(gin.Logger())
+
+	r.Use(gin.Recovery())
+
+	gin.SetMode(setting.RunMode)
+
+	apiv1 := r.Group("/api/v1")
+	{
+		apiv1.GET("/tags", v1.GetTags)
+		apiv1.POST("/tags", v1.AddTag)
+		apiv1.PUT("/tags", v1.EdisTag)
+		apiv1.DELETE("/tags", v1.DeleteTag)
+	}
+
+	return r
+}
